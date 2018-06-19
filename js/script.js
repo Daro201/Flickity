d070cb668a66b9fa1e9b456ae6c2cf249a6ff715
Adrian Meller <melleradria@gmail.com>
2018-06-19 22:59:59 +0200

    Finish task 10.1

--- js/script.js
+++ js/script.js
@@ -1,15 +1,22 @@
-var elem = document.querySelector('.main-carousel');
-var flkty = new Flickity( elem, {
+var elem = document.querySelector('.carousel');
+var flkty = new Flickity(elem, {
   // options
   cellAlign: 'left',
   contain: true,
   hash: true,
-  pageDots: false
+  pageDots: false 
 });
 
-// element argument can be a selector string
-//   for an individual element
-var flkty = new Flickity( '.main-carousel', {
-  // options
-  
+var buttonGroup = document.querySelector('.button');
+
+buttonGroup.addEventListener( 'click', function( event ) {
+  var selector = event.target.getAttribute('data-selector');
+  flkty.selectCell( selector );
+});
+
+var progressBar = document.querySelector('.progress-bar')
+
+flkty.on( 'scroll', function( progress ) {
+  progress = Math.max( 0, Math.min( 1, progress ) );
+  progressBar.style.width = progress * 100 + '%';
 });
